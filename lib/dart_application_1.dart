6b9187396697b4ab0573f721a76a7e1c6fe38d14
fouzan. p <fouzanparammal2@gmail.com>
2024-09-10 15:38:49 +0530

    added

---
+++ lib/dart_application_1.dart
@@ -0,0 +1,100 @@
+// //  named parameter 
+//  void namedParameter({required String name, int? age }){
+//   print("hello $name you are $age years old");
+// }
+// void main(){
+//   namedParameter(name: "Fouzan ",age: 4);
+
+// }
+
+
+
+// optional parameter 
+
+// void optionalParameter( String name, [int? age] ){
+//   print("hello $name you are $age years old");
+
+//   if(age!=null){
+//     print('hello $name you are $age years old');
+//   }else{
+//     print('hello $name');
+//   }
+// }
+// void main(){
+//   optionalParameter('fouzan',59);
+// }
+
+
+
+// default parameter 
+
+// void defaultParameter(String name ,{int? age = 29 }){
+//   print('hai $name you are $age years old ');
+// }
+
+// void main(){
+//   defaultParameter('fouzan');
+//   defaultParameter('fouzan',age:  21);
+// }
+
+
+
+
+// requred parameter
+
+// void requiredParameter({required String name, int? age }){
+//   print('hello $name your are $age years old');
+// }
+
+// void main(){
+//   requiredParameter(name: 'fouzan',age: 34);
+//   requiredParameter(); 
+// }
+
+
+
+// while loop 
+
+// void main(){
+//   int i =1;
+//   while(i<=5){
+//     print(i);
+//     i++;
+//   }
+// }
+
+
+
+// do while 
+
+
+// void main(){
+// int i=1;
+// do{
+//   print(i);
+//   i++;
+// }while(i<=5);
+// }
+
+
+// for in 
+
+// void main(){
+// List<String> fruits =['banana, apple ,grape'];
+// for(var fruit in fruits){
+//   print(fruit);
+// }
+// }
+
+
+// forEach
+
+// void main(){
+// List<dynamic> numbers =[1,3,4,5,6,7];
+// numbers.forEach((number){
+//   print(number);
+// });
+// }
+
+
+
